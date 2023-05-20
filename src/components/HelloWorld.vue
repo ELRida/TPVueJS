21acb2366f1ed167c1dbaee5abfbdb7cb328b7c2
Nassim-din <90925523+Nassim-din@users.noreply.github.com>
2023-05-20 10:00:07 +0200

    Avancement

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -21,56 +21,55 @@
         <tr>
           <th scope="col">#</th>
           <th scope="col">Tâches</th>
-          <th scope="col">Date de création</th>
+          <th scope="col">À faire pour le</th>
           <th scope="col">Statut</th>
           <th scope="col">Actions</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
+        <tr v-for="(task, index) in filteredTodo" :key="index">
           <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td> <v-icon small class="mr-2" > mdi-pencil </v-icon>
-                    <v-icon small> mdi-delete </v-icon>
+          <td>{{task.name}}</td>
+          <td>{{ task.date }}</td>
+          <td>{{ task.status }}</td>
+          <td> <v-icon small class="mr-2" @click="dialogEditTask(index)"> mdi-pencil </v-icon>
+                    <v-icon small @click="deleteTask(index)"> mdi-delete </v-icon>
 </td>
         </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-          <td> <v-icon small class="mr-2"> mdi-pencil </v-icon>
-                    <v-icon small > mdi-delete </v-icon>
-</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Larry</td>
-          <td>the Bird</td>
-          <td>@twitter</td>
-          <td> <v-icon small class="mr-2"> mdi-pencil </v-icon>
-                    <v-icon small> mdi-delete </v-icon>
-</td>
-        </tr>
+    
+    
       </tbody>
     </table>
     <!-- DIALOG -->
+        <!-- ADD DIALOG -->
     <v-dialog
       class="dialogDeleteTemplate"
       v-model="addTodo"
       width="500"
       :retain-focus="false"
     >
-      <v-card>
+      <v-card class="dialog_add">
         <v-card-title style="justify-content: center">
           <v-sheet class="mb-4 mt-4 text-center title-bold text-delete">
-            Êtes-vous sûr de vouloir supprimer ce template ?
+            Ajouter une tâche
           </v-sheet>
         </v-card-title>
+        <div class="box_add">
+      <v-text-field
+        placeholder="Nom de la tâche"
+        class="add_todo"
+        v-model="task"
+      ></v-text-field>
+      <div >
+        
+        <v-text-field class="datapicker_todo" type="date" label="Date" v-model="due"></v-text-field>
 
-        <v-card-actions>
+
+      
+      </div>
+    </div>
+        <v-card-actions class="text-center">
+          
           <v-spacer class="confirmChoice">
             <v-btn
               style="margin-right: 15px"
@@ -82,7 +81,7 @@
             <v-btn
               class="btn-action"
               text
-              @click="deleteRoom()"
+              @click="submitTask()"
               :loading="loadingDelete"
             >
               Valider</v-btn
@@ -91,71 +90,160 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
+
+
+       <!-- EDIT DIALOG -->
+       <v-dialog
+      class="dialogDeleteTemplate"
+      v-model="editTodo"
+      width="500"
+      :retain-focus="false"
+    >
+      <v-card class="dialog_add">
+        <v-card-title style="justify-content: center">
+          <v-sheet class="mb-4 mt-4 text-center title-bold text-delete">
+            Modification de tâche
+          </v-sheet>
+        </v-card-title>
+        <div class="box_add">
+      <v-text-field
+        placeholder="Nom de la tâche"
+        class="add_todo"
+        v-model="task"
+      ></v-text-field>
+      <div >
+        
+        <v-text-field class="datapicker_todo" type="date" label="Date" v-model="due"></v-text-field>
+
+
+      
+      </div>
+    </div>
+        <v-card-actions class="text-center">
+          
+          <v-spacer class="confirmChoice">
+            <v-btn
+              style="margin-right: 15px"
+              class="btn-action-cancel"
+              text
+              @click="closeDialogEdit()"
+              >Retour</v-btn
+            >
+            <v-btn
+              class="btn-action"
+              text
+              @click="editTask()"
+            >
+              Valider</v-btn
+            >
+          </v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <!--DELETE-->
+
+  
   </v-container>
 </template>
 
 <script>
 export default {
+  
   name: "HelloWorld",
+ 
 
-  data: () => ({
-    ecosystem: [
+  data() {
+  return {
+    task:'',
+    editedTask: null,
+    tasks: [
       {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader/tree/next",
+        name: 'Oui monsieur',
+        date: new Date().toLocaleDateString('fr-FR'),
+        status: 'to-do',
       },
       {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify/tree/next",
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify",
-      },
-    ],
-    importantLinks: [
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com",
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify",
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs",
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify",
-      },
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com",
-      },
-      {
-        text: "Roadmap",
-        href: "https://vuetifyjs.com/introduction/roadmap/",
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
-      },
+        name: 'Oui',
+        date: new Date().toLocaleDateString('fr-FR'),
+        status: 'in-progress',
+      }
     ],
     addTodo: false,
-  }),
+    editTodo: false,
+    due: null,
+    search_room: "",
+    date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+  };
+},
   methods: {
     addTodoList() {
       this.addTodo = true;
+      this.task = '';
+      this.due = null; 
+
+  if (this.date === this.due) {
+    this.date = null;
+  }
+
     },
     closeDialog() {
-      this;
       this.addTodo = false;
     },
+    closeDialogEdit() {
+      this.editTodo = false;
+    },
+    submitTask() {
+  if (this.task.length === 0) return;
+
+  this.tasks.push({
+    name: this.task,
+    date: this.due, 
+    status: 'todo'
+  });
+
+  this.task = '';
+  this.due = null; 
+
+  if (this.date === this.due) {
+    this.date = null;
+  }
+},
+dialogEditTask(index){
+  this.editTodo = true;
+  this.task = this.tasks[index].name;
+  
+  if (index < 2) { 
+    this.due = new Date().toLocaleDateString('fr-FR');
+  } else {
+    this.due = this.tasks[index].date;
+  }
+  
+  this.editedTask = this.tasks[index];
+},
+editTask() {
+  if (!this.editedTask) return;
+
+  this.editedTask.name = this.task;
+  this.editedTask.date = this.due;
+  this.task = '';
+  this.due = null;
+  this.editedTask = null;
+  this.editTodo = false;
+},
+    deleteTask(index){
+      this.tasks.splice(index, 1 );
+    }
   },
+
+  computed:{
+    filteredTodo() {
+      return this.tasks.filter((task) =>
+        task.name.toLowerCase().includes(this.search_room.toLowerCase())
+      );
+    },
+  }
+
 };
 </script>
 
@@ -186,5 +274,24 @@ export default {
   display: flex;
   height: 50px;
   color: white;
+}
+.add_todo .v-input__control {
+  min-width: 150px;
+  max-width: 180px;
+}
+.add_todo .v-field__overlay {
+  background: none;
+}
+.dialog_add{
+  padding: 12px;
+}
+.box_add{
+  display: flex;
+}
+.box_add .v-input--horizontal{
+  width: 240px;
+}
+.datapicker_todo .v-field__overlay{
+  background: none;
 }
 </style>
